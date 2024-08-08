6e06b8d5a4b2f15c5f19cedf8bbe2d3a1080b51a
Claudia Rodrigues <cacauabreu2013@gmail.com>
2024-08-08 15:13:13 -0300

    feat: Adicionar permissoes de entre projetos e configs

--- apps/bingo-app/src/app/app.routes.ts
+++ apps/bingo-app/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Route } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: NxWelcomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'ui',
+    loadComponent: () =>
+      import('@bingo-player-one-monorepo/bingo-ui').then(
+        (m) => m.BingoUiComponent
+      ),
+  },
+];
