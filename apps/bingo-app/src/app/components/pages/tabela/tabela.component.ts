b200b57f0c177af161b53cd25f25f970cc06e70f
Claudia Rodrigues <cacauabreu2013@gmail.com>
2024-08-09 16:00:34 -0300

    refactor: Refatorar alguns detalhes de tabela

--- apps/bingo-app/src/app/components/pages/tabela/tabela.component.ts
+++ apps/bingo-app/src/app/components/pages/tabela/tabela.component.ts
@@ -3,11 +3,12 @@ import { Component, OnInit } from '@angular/core';
 // eslint-disable-next-line @nx/enforce-module-boundaries
 import { ButtonUIComponent } from '@bingo-player-one-monorepo/bingo-ui';
 import { JogoService } from '../../../infra/services/Jogo.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tabela',
   standalone: true,
-  imports: [CommonModule, ButtonUIComponent],
+  imports: [CommonModule, ButtonUIComponent, RouterLink],
   templateUrl: './tabela.component.html',
   styleUrl: './tabela.component.scss',
 })
